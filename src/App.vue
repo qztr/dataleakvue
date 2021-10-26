fad85f69a317956efaada7de2daa86bd30af2fd3
White wine <vladiska777@gmail.com>
2021-10-26 12:02:52 +0400

    init

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,46 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div class="mian grid grid-rows-3 relative mx-12">
+      <div
+        class="header h-32 flex flex-col w-auto justify-center align-middle mt-28 mb-40 "
+      >
+        <p class=" text-redy text-5xl font-bold uppercase ">
+          Share your data for free!
+        </p>
+      </div>
+      <div
+        class="content h-32 flex flex-col w-auto justify-center align-middle  my-20
+        text-xl"
+      >
+        <p>
+          You can share your data absolutely for free and get nothing back! We
+          already know your
+        </p>
+        <div class="">
+          <br />
+          1) IP
+          <br />
+          2) GEO
+        </div>
+        <br />
+        <p>
+          and we will store this data at blockchain (i). Later we will make your
+          portrait and sell it to third party services. Thank you for sharing!
+          Would you like to provide us more infromation?
+        </p>
+      </div>
+      <div class="footer h-32 flex w-full justify-center align-middle">
+        footer
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: {},
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
